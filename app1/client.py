ec7ed9ea96040df7f32603be80ab726656565fc2
lucaspsacchi <lucaspsacchi12345@hotmail.com>
2019-06-25 11:21:52 -0300

    Alteração do app1

--- app1/client.py
+++ app1/client.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #client_sock.py
 import socket
+import os
 
 HOST = 'localhost' #coloca o host do servidor
 PORT = 50000
@@ -10,14 +14,42 @@ def client():
     sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
 
-    arq = open('c:/UFSCAR/7º Semestre/Top Fábio/Trab2-ClientServer/exemplo.txt', 'rb')
+    while True:
+      print("Escolha um dos 3 arquivos:")
+      print("0 - sair")
+      print("1 - exemplo1.txt")
+      print("2 - exemplo2.txt")
+      print("3 - exemplo3.txt")
+      aux = input()
 
-    # Envia a mensagem pelo socket para um ip e uma porta destino
-    for info in arq.readlines():
+      if (aux == 1):
+        nome = "exemplo1.txt"
+      elif (aux == 2):
+        nome = "exemplo2.txt"
+      else:
+        nome = "exemplo3.txt"
+
+      # Sai da aplicação
+      if aux == 0:
+        break
+
+      # Abre o arquivo para leitura
+      arq = open(nome, 'rb')
+
+      # Envia o nome do arquivo
+      sock.sendto(nome, (HOST, PORT))
+
+      # Envia a mensagem pelo socket para um ip e uma porta destino
+      for info in arq.readlines():
         print(str(info))
         sock.sendto(info, (HOST, PORT))
-    
-    arq.close()
+
+      # Fechando o arquivo
+      info = "close"
+      sock.sendto(info, (HOST, PORT))
+
+      arq.close()
+
     sock.close()
 
 client()
--- app1/server.py
+++ app1/server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #serv_sock.py
 import socket
 
@@ -6,25 +9,29 @@ PORT = 50000
 
 def server():
 
-    # Cria socket UDP
-    sock = socket.socket(socket.AF_INET, # Internet
-                        socket.SOCK_DGRAM) # UDP
-    sock.bind((HOST, PORT))
+  # Cria socket UDP
+  sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+  sock.bind((HOST, PORT))
 
-    arq = open('c:/UFSCAR/7º Semestre/Top Fábio/Trab2-ClientServer/teste.txt', 'w')
+  while True:
+    print("Esperando envio...")
+
+    # Recebe o nome do arquivo que está enviando
+    nome, addr = sock.recvfrom(1024)
+    arq = open('envio/' + nome, 'w')
 
     while True:
-        print("Esperando envio...")
-        # Define o tamanho máximo da mensagem
-        info, addr = sock.recvfrom(1024) # Tamanho do buffer é de 1024 bytes
-        # while info[-1:] == '\0':
-        #     info = info[:-1]
-        if not info: # Não está saindo
-            break
-        # Decodifica a mensagem em ...
-        var = info.decode('utf-8')
-        print('Type:' + str(type(var)) + '\nvar: ' +  var)
-        arq.write(var)
+      # Define o tamanho máximo da mensagem
+      info, addr = sock.recvfrom(1024) # Tamanho do buffer é de 1024 bytes
+      # while info[-1:] == '\0':
+      #     info = info[:-1]
+      if info == "close":
+        break
+      # Decodifica a mensagem em ...
+      var = info.decode('utf-8')
+      print('Type:' + str(type(var)) + '\nvar: ' +  var)
+      arq.write(var)
 
     arq.close()
 
