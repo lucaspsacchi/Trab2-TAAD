5d7883304cfde2b7ed372ac3bddb9c4588fe6067
Lucas Penteado <lucaspsacchi12345@hotmail.com>
2019-07-01 12:23:50 -0300

    Criação de um script para instalar as ferramentas

---
+++ prometheus/script.sh
@@ -0,0 +1,5 @@
+sudo docker build -t my-prometheus .
+sudo docker run -p 9090:9090 --restart=always --detach=true --name=prometheus my-prometheus
+sudo docker run -d --restart=always --net="host" --pid="host" --publish=9100:9100 --detach=true --name=node-exporter -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter --path.rootfs /host
+sudo docker run --restart=always --volume=/:/rootfs:ro --volume=/var/run:/var/run:ro --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --publish=8080:8080 --detach=true --name=cadvisor google/cadvisor:latest
+sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
